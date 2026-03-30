4a37eb8a5d7f1b2454f2e3cd759572dfb39fb6d2
Hijas <hijasvengeri123@gmail.com>
2026-03-30 15:20:04 +0530

    removed under construction page

--- src/app/admin/under-construction/page.js
+++ src/app/admin/under-construction/page.js
@@ -1,10 +1,10 @@
-import { Suspense } from "react";
-import UnderConstructionContent from "./UnderConstructionContent";
+// import { Suspense } from "react";
+// import UnderConstructionContent from "./UnderConstructionContent";
 
-export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <UnderConstructionContent />
-    </Suspense>
-  );
-}
+// export default function Page() {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <UnderConstructionContent />
+//     </Suspense>
+//   );
+// }
