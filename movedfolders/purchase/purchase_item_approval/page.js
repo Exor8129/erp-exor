9c7d494a625f0258d17ab0f1d038c2c7d101930c
Hijas <hijasvengeri123@gmail.com>
2026-03-30 16:06:25 +0530

    moved folders

--- movedfolders/purchase/purchase_item_approval/page.js
+++ movedfolders/purchase/purchase_item_approval/page.js
@@ -183,7 +183,7 @@
 import React, { useEffect, useState } from "react";
 import { Table, Tag, Button, Space, message, Typography, Select, Card, Tabs } from "antd";
 import { CheckCircleOutlined, CloseCircleOutlined, EditOutlined } from "@ant-design/icons";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../../../src/app/lib/supabase";
 import { useRouter } from "next/navigation";
 
 const { Title } = Typography;
